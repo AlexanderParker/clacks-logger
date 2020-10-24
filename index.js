b207aac075f09afea05794b9a586df630a728fdd
Alex Parker <alexofparker@gmail.com>
2020-10-25 01:59:26 +0800

    Migrate logger code from core clacks-p2p repo

---
+++ index.js
@@ -0,0 +1,57 @@
+/*
+	Message logger plugin for clacks-p2p
+	Implements a basic message logger, useful for profiling and debugging
+*/
+
+const fs = require('fs')
+/*
+	Initialise the logger plugin with options.
+
+	Options format:
+	{
+		directory: 'base/log/directory',
+		type: 'message|announce|all'
+	}
+
+	Default options:
+
+	directory: "./logger"
+	type: "./all"
+*/
+
+module.exports = function(options) {
+	var logDirectory = "./logger",
+		logType = "all"
+
+	// Apply options overrides
+	if (!!options && !!options.directory) logDirectory = options.directory
+	if (!!options && !!options.type) logDirectory = options.type
+
+	// Check logger directory
+	if (!fs.existsSync(logDirectory)) {
+		fs.mkdirSync(logDirectory, {recursive: true});
+	}
+
+	return function(peer, payload, req, res) {
+		// Crude way to ensure valid identifier
+		if (!peer || !peer.identifier || !peer.identifier.match(/[A-Fa-f0-9]{64}/)) return		
+
+		// Only log messages
+		if (logType != 'all' && (!payload.type || payload.type != logType)) return
+
+		// Message is logged to filesystem in logir/identifier/timestamp
+		var peerLogDirectory = logDirectory + '/' + peer.identifier
+			peerLogFilename = Date.now()
+
+		// Ensure log directory exists (is sync for now, may improve later)
+		if (!fs.existsSync(peerLogDirectory)) {
+			fs.mkdirSync(peerLogDirectory);
+		}
+
+		// Log message to the file (is sync for now, may improve later)
+		fs.writeFileSync(peerLogDirectory + '/' + peerLogFilename, JSON.stringify({
+			peer: peer,
+			payload: payload
+		}))
+	}
+}
